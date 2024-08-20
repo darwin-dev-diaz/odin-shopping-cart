d3854aa8dd451ea4a58d390a0a880cc025c3952c
Darwin Diaz <152736334+darwin-dev-diaz@users.noreply.github.com>
2024-08-20 16:22:44 -0400

    Get carousel nav buttons to work

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,25 +1,36 @@
 import styles from "./carousel.module.css";
 import ForwardArrowSVG from "../../assets/ForwardArrowSVG";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 import BackgroundOne from "../../assets/carousel_1.jpg";
-import BackgroundTwo from "../../assets/carousel_2.jpg"
-import BackgroundThree from "../../assets/carousel_3.jpg"
+import BackgroundTwo from "../../assets/carousel_2.jpg";
+import BackgroundThree from "../../assets/carousel_3.jpg";
 
 const Carousel = () => {
+  const [carouselIndex, setCarouselIndex] = useState(0);
+  const backgroundArray = [BackgroundOne, BackgroundTwo, BackgroundThree];
+  const changeCarouselIndex = (direction = 1) => {
+    if (direction > 0 && carouselIndex === 2) {
+      setCarouselIndex(0);
+    } else if (direction < 0 && carouselIndex === 0) {
+      setCarouselIndex(2);
+    } else {
+      setCarouselIndex(carouselIndex + direction);
+    }
+  };
   return (
     <div
       className={styles.carousel}
-      style={{ backgroundImage: `url(${BackgroundTwo})` }}
+      style={{ backgroundImage: `url(${backgroundArray[carouselIndex]})` }}
     >
-      {/* <div
-        className={styles.carousel_image_container}
-        
-      ></div> */}
-      <div className={styles.forward_arrow}>
+      <div
+        className={styles.forward_arrow}
+        onClick={() => changeCarouselIndex(1)}
+      >
         <ForwardArrowSVG />
       </div>
-      <div className={styles.back_arrow}>
+      <div className={styles.back_arrow} onClick={() => changeCarouselIndex(-1)}>
         <ForwardArrowSVG />
       </div>
       <Link to="/store" className={styles.carousel_shop_button}>
